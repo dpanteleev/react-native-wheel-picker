a771079965cd6ed21a6be9c6379d83979f269d48
Denis Panteleev <denis.panteleev@itexus.com>
2018-06-18 13:43:54 +0300

    Force linecolor

--- android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
@@ -58,8 +58,8 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
 
         Paint paint = new Paint();
         paint.setColor(Color.GRAY);
-        int colorFrom =  0x00FFFFFF;
-        int colorTo = Color.WHITE;
+        int colorFrom =  Color.GRAY;
+        int colorTo = Color.GRAY;
         LinearGradient linearGradientShader = new LinearGradient(rectCurItem.left, rectCurItem.top, rectCurItem.right/2, rectCurItem.top, colorFrom, colorTo, Shader.TileMode.MIRROR);
         paint.setShader(linearGradientShader);
         canvas.drawLine(rectCurItem.left, rectCurItem.top, rectCurItem.right, rectCurItem.top, paint);
